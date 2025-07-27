5c77e91f01ac4fb32164e2f22f17d4b4fffea1ac
zhufucdev <zhufuzhufu1@gmail.com>
2025-07-27 14:48:48 +0800

    add command palate

--- nvim/init.lua
+++ nvim/init.lua
@@ -180,6 +180,13 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
   'stevearc/dressing.nvim',
+  'junegunn/fzf',
+  {
+    'junegunn/fzf.vim',
+    config = function()
+      vim.keymap.set({ 'n', 'i' }, '<D-P>', '<cmd>Commands<CR>')
+    end,
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
