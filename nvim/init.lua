e23c7d2ad5df91c09a3579e1470b184fcd7499b6
zhufucdev <zhufuzhufu1@gmail.com>
2025-07-27 11:35:18 +0800

    add more lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -469,6 +469,8 @@ require('lazy').setup({
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
+        kotlin_lsp = {},
+        prismals = {},
 
         lua_ls = {
           -- cmd = { ... },
