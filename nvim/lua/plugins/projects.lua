b3b1f9dddb09a7e5afad1b3d33e3b95936a86475
zhufucdev <29772292+zhufucdev@users.noreply.github.com>
2025-08-06 13:59:29 +0800

    turn off automatic mode

--- nvim/lua/plugins/projects.lua
+++ nvim/lua/plugins/projects.lua
@@ -2,7 +2,9 @@ return {
   'ahmedkhalf/project.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim' },
   config = function()
-    require('project_nvim').setup {}
+    require('project_nvim').setup {
+      manual_mode = true,
+    }
     vim.keymap.set('', '<D-O>', '<cmd>Telescope projects<CR>')
   end,
 }
