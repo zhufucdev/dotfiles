87e63794c85b106a70ef004042460c262fa94302
zhufucdev <zhufuzhufu1@gmail.com>
2025-11-09 16:58:32 +0800

    switch to pyrefly

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ local function get_servers()
   return {
     clangd = {},
     gopls = {},
-    pyright = {},
+    pyrefly = {},
     -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
