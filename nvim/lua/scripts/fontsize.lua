448efffc4f220d65a5fe28d8400acee718a12af6
zhufucdev <29772292+zhufucdev@users.noreply.github.com>
2025-12-18 11:02:43 +0800

    fontsize supports neovide
    (cherry picked from commit d8ad9fad2f3f8919dc729484564c9decced31a6e)

--- nvim/lua/scripts/fontsize.lua
+++ nvim/lua/scripts/fontsize.lua
@@ -3,11 +3,19 @@ local function get_groups()
 end
 
 vim.keymap.set('n', '<D-=>', function()
-  local groups = get_groups()
-  vim.o.guifont = groups[1] .. ':h' .. groups[2] + 1
+  if vim.g.neovide then
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1
+  else
+    local groups = get_groups()
+    vim.o.guifont = groups[1] .. ':h' .. groups[2] + 1
+  end
 end)
 
-vim.keymap.set('n', '<D-->', function ()
-  local groups = get_groups()
-  vim.o.guifont = groups[1] .. ':h' .. groups[2] - 1
+vim.keymap.set('n', '<D-->', function()
+  if vim.g.neovide then
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1
+  else
+    local groups = get_groups()
+    vim.o.guifont = groups[1] .. ':h' .. groups[2] - 1
+  end
 end)
