3dc7625c1fec1e2f8537c0baec774856d24750dd
zhufucdev <zhufuzhufu1@gmail.com>
2025-12-27 16:43:24 +0800

    make autocompletion menu super long

--- nvim/lua/plugins/autocompletion.lua
+++ nvim/lua/plugins/autocompletion.lua
@@ -74,6 +74,9 @@ return { -- Autocompletion
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      menu = {
+        max_height = 40,
+      },
     },
 
     sources = {
