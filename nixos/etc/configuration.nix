ce9f2536f869bc0df7d0909c3ce53ca3beed5d25
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-02 22:12:09 +0800

    add steam for nixos

--- nixos/etc/configuration.nix
+++ nixos/etc/configuration.nix
@@ -166,6 +166,8 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  programs.steam.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
