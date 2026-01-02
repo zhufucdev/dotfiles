5893721785599d2fcbb1d6967ec15430ac7f2bb5
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-02 20:01:19 +0800

    add nixos configurations

---
+++ nixos/etc/configuration.nix
@@ -0,0 +1,236 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use LTS kernel
+  boot.kernelPackages = pkgs.linuxPackages_6_12;
+
+  # ZFS support
+  networking = {
+    hostId = "e99f7b6a";
+
+    hostName = "functionaltux"; # Define your hostname.
+
+    # Configure network connections interactively with nmcli or nmtui.
+    networkmanager.enable = true;
+
+    # Configure network proxy if necessary
+    proxy.default = "http://localhost:7890";
+    proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+    bridges.br0.interfaces = [
+      "enp37s0"
+      "enp38s0"
+    ];
+    useDHCP = false;
+
+    interfaces.br0.ipv4.addresses = [
+      {
+        address = "192.168.124.10";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = "192.168.124.1";
+    nameservers = [ "192.168.124.1" ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "Asia/Shanghai";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Samba share zfs pool
+  services.samba = {
+    enable = true;
+    package = pkgs.sambaFull;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "functionaltux";
+        "netbios name" = "functionaltux";
+        "security" = "user";
+
+        "guest account" = "nobody";
+
+        "usershare path" = "/var/lib/samba/usershares";
+        "usershare max shares" = "100";
+        "usershare allow guests" = "yes";
+        "usershare owner only" = "no";
+      };
+    };
+    openFirewall = true;
+  };
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    nssmdns6 = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      addresses = true;
+    };
+    allowInterfaces = [ config.networking.interfaces.br0.name ];
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.steve = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      "uinput"
+    ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+cV1MGvbTix/2rL1cUMfLbbhBXwOutUwNUNYle+c5F zhufu@zhufusmbp.local"
+    ];
+  };
+  # programs.firefox.enable = true;
+  programs.zsh.enable = true;
+
+  # List packages installed in system profile.
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+    git
+    rsync
+    gparted
+    zip
+    unzip
+    gcc
+    nixd
+    nixfmt
+    file
+    lsof
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # Enable mihomo service
+  services.mihomo = {
+    enable = true;
+    configFile = "/etc/mihomo/config.yml";
+    tunMode = true;
+  };
+
+  # Minimal Gnome desktop environment
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+  services.gnome = {
+    core-apps.enable = false;
+    core-developer-tools.enable = false;
+    games.enable = false;
+  };
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+    gnome-user-docs
+  ];
+  services.displayManager.autoLogin.enable = true;
+  services.displayManager.autoLogin.user = "steve";
+
+  services.sunshine = {
+    enable = true;
+    autoStart = true;
+    capSysAdmin = true; # only needed for Wayland -- omit this when using with Xorg
+    openFirewall = true;
+    package = pkgs.sunshine.override { cudaSupport = true; };
+  };
+  hardware.uinput.enable = true;
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+  nix.settings = {
+    experimental-features = "nix-command flakes";
+    # CUDA cache
+    substituters = [ "https://cache.nixos-cuda.org" ];
+    trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
+  };
+
+  # Enable unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
---
+++ nixos/home-manager/home.nix
@@ -0,0 +1,119 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "steve";
+  home.homeDirectory = "/home/steve";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    rustup
+    go
+    python3
+    nodejs_24
+    pnpm
+    pyenv
+    mcfly
+    tmux
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/steve/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    initContent = ''
+      export MCFLY_FUZZY=2
+      eval "$(pyenv init -)"
+      eval "$(mcfly init zsh)"
+      bindkey ';3C' forward-word
+      bindkey ';3D' backward-word
+
+      autoload -U select-word-style
+      select-word-style bash
+      export WORDCHARS='.-'
+    '';
+
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        {
+          name = "0i0/0i0.zsh-theme";
+          tags = [
+            "as:theme"
+            "depth:1"
+          ];
+        } # Installations with additional options. For the list of options, please refer to Zplug README.
+        {
+          name = "zsh-users/zsh-history-substring-search";
+          tags = [ "as:plugin" ];
+        }
+      ];
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "zhufucdev";
+        email = "zhufuzhufu1@gmail.com";
+      };
+    };
+  };
+}
