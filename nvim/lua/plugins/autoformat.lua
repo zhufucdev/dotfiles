bf559fbf59190ac33b0be25e41cda4abfa2ab312
zhufucdev <zhufuzhufu1@gmail.com>
2026-01-11 09:06:35 +0800

    fix autoformat

--- nvim/lua/plugins/autoformat.lua
+++ nvim/lua/plugins/autoformat.lua
@@ -1,6 +1,6 @@
-local use_prettier = { 'prettierd', 'prettier', stop_after_first = true }
+local use_prettier = { 'prettier', 'prettierd', stop_after_first = true }
 
-local function use_fontend()
+local function use_frontend()
   if vim.fn.filereadable 'eslint.config.js' == 1 or vim.fn.filereadable 'eslint.config.mjs' == 1 then
     return { 'eslint_d' }
   end
@@ -29,15 +29,16 @@ return { -- Autoformat
       python = { 'isort', 'black', stop_after_first = false },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
-      javascript = use_fontend,
-      typescript = use_fontend,
-      typescriptreact = use_fontend,
-      astro = use_fontend,
-      markdown = use_fontend,
-      json = use_fontend,
+      javascript = use_frontend,
+      typescript = use_frontend,
+      typescriptreact = use_frontend,
+      astro = use_frontend,
+      markdown = use_frontend,
+      mdx = use_frontend,
+      json = use_frontend,
       yaml = use_prettier,
-      css = use_fontend,
-      vue = use_fontend,
+      css = use_frontend,
+      vue = use_frontend,
     },
   },
 }
