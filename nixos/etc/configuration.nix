757efb8caa833742c0b3daeacc714980bf5c55b5
zhufucdev <zhufuzhufu1@gmail.com>
2026-02-19 20:23:35 +0800

    add ollama to nixos

--- nixos/etc/configuration.nix
+++ nixos/etc/configuration.nix
@@ -254,6 +254,11 @@
     };
   };
 
+  services.ollama = {
+    enable = true;
+    package = pkgs.ollama-cuda;
+  };
+
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
