15e526fe3629c9d7a61835485f9415c3e64a67b5
zhufucdev <zhufuzhufu1@gmail.com>
2026-04-16 12:56:57 +0800

    add noice

--- nvim/init.lua
+++ nvim/init.lua
@@ -427,6 +427,29 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
   'davidmh/mdx.nvim',
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+    },
+    opts = {
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+      cmdline = {
+        opts = {
+          position = '100%',
+          anchor = 'NE',
+        },
+      },
+    },
+  },
   { import = 'plugins' },
 }, {
   ui = {
