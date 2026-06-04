e255c6afaa25dc35ac17694a8518e40879be4ce9
zhufucdev <zhufuzhufu1@gmail.com>
2026-06-04 14:22:06 +0800

    change br0 mtu to 9000

--- nixos/etc/configuration.nix
+++ nixos/etc/configuration.nix
@@ -38,12 +38,15 @@
     ];
     useDHCP = false;
 
-    interfaces.br0.ipv4.addresses = [
-      {
-        address = "192.168.124.10";
-        prefixLength = 24;
-      }
-    ];
+    interfaces.br0 = {
+      ipv4.addresses = [
+        {
+          address = "192.168.124.10";
+          prefixLength = 24;
+        }
+      ];
+      mtu = 9000;
+    };
     defaultGateway = "192.168.124.1";
     nameservers = [ "192.168.124.1" ];
   };
