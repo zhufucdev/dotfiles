b0bb99e68d9a16ee4da8a0ab165ea340647d689c
zhufucdev <zhufuzhufu1@gmail.com>
2026-06-13 09:17:46 +0800

    add floating context and treesitter indent

--- nvim/init.lua
+++ nvim/init.lua
@@ -390,21 +390,18 @@ require('lazy').setup({
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = {},
-      },
-      indent = { enable = true },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    config = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { '<filetype>' },
+        callback = function()
+          -- Syntax highlighting supported by neovim
+          vim.treesitter.start()
+          -- Indent with treesitter
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
   },
   'davidmh/mdx.nvim',
   {
---
+++ nvim/lua/plugins/context.lua
@@ -0,0 +1,4 @@
+return {
+  'nvim-treesitter/nvim-treesitter-context',
+  opts = { multiwindow = true, line_numbers = false, multiline_threshold = 2 },
+}
