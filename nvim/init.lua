4f28b1f6b0306f1e876a5eba8818203ae598fb84
zhufucdev <zhufuzhufu1@gmail.com>
2026-06-14 21:42:30 +0800

    remove noice cause its buggy

--- nvim/init.lua
+++ nvim/init.lua
@@ -404,48 +404,6 @@ require('lazy').setup({
     end,
   },
   'davidmh/mdx.nvim',
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-      'MunifTanjim/nui.nvim',
-    },
-    opts = {
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-        },
-        message = {
-          enabled = false,
-        },
-        progress = {
-          enabled = false,
-        },
-      },
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = false, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
-      },
-      cmdline = {
-        opts = {
-          position = '100%',
-          anchor = 'NE',
-        },
-      },
-      notify = {
-        -- vim.notify like notifications
-        -- default: true
-        enabled = false,
-      },
-    },
-  },
   { import = 'plugins' },
 }, {
   ui = {
